6e1ff2043a0ce44e19c3b93685208d37490f6627
E. Mert EREN <37554755+merteren97@users.noreply.github.com>
2026-08-09 00:42:48 +0300

    target Android 17.1 compile SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,11 @@ plugins {
 
 android {
     namespace = "com.atrishub.atriscast"
-    compileSdk = 37
+    compileSdk {
+        version = release(37) {
+            minorApiLevel = 1
+        }
+    }
 
     defaultConfig {
         applicationId = "com.atrishub.atriscast"
