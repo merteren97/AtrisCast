62c0ee8d11e96510c035f1afff050383af96f4de
E. Mert EREN <37554755+merteren97@users.noreply.github.com>
2026-08-09 17:58:38 +0300

    Bump AtrisCast to alpha07

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.atrishub.atriscast"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.1.0-alpha06"
+        versionCode = 7
+        versionName = "0.1.0-alpha07"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
